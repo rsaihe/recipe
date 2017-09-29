bde305edc2a20a93f46a794d8e97bd68558bc80b
xithiox <xithiox@gmail.com>
2017-09-29 17:12:41 -0500

    Added support for custom text

--- markov.js
+++ markov.js
@@ -1,3 +1,6 @@
+var probs_cust;
+var probs_cust_char;
+
 function markov(prev, probs) {
     if (probs.hasOwnProperty(prev)) {
         return probs[prev][Math.floor(Math.random() * probs[prev].length)];
@@ -6,34 +9,79 @@ function markov(prev, probs) {
     }
 }
 
-function markov_gen(count, probs, prev_count) {
-    var prevs = new Array(prev_count).fill('');
-    var wordlist = [];
-
-    for (var i = 0; i < count; ++i) {
-        var prev = prevs.join(' ');
-        var word = markov(prev, probs[prev_count - 1]);
-        prevs.shift();
-        prevs.push(word);
-        wordlist.push(word);
+function scan(text, probs) {
+    if (typeof probs === 'undefined') {
+        probs = [{}, {}, {}, {}];
     }
 
-    return wordlist.join(' ');
+    var words = text.split(/\s+/);
+    for (var i = 0; i < 4; ++i) {
+        var prevs = new Array(i + 1);
+        prevs.fill('');
+        for (var w = 0; w < words.length; ++w) {
+            var word = words[w];
+            var prev = prevs.join(' ');
+            if (!probs[i].hasOwnProperty(prev)) {
+                probs[i][prev] = [];
+            }
+            probs[i][prev].push(word);
+            prevs.shift();
+            prevs.push(word);
+        }
+    }
+
+    return probs;
 }
 
-function markov_gen_char(count, probs, prev_count) {
+function scanChar(text, probs) {
+    if (typeof probs === 'undefined') {
+        probs = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+    }
+
+    var words = text.split('');
+    for (var i = 0; i < 10; ++i) {
+        var prevs = new Array(i + 1);
+        prevs.fill('');
+        for (var w = 0; w < words.length; ++w) {
+            var word = words[w];
+            var prev = prevs.join('');
+            if (!probs[i].hasOwnProperty(prev)) {
+                probs[i][prev] = [];
+            }
+            probs[i][prev].push(word);
+            prevs.shift();
+            prevs.push(word);
+        }
+    }
+
+    return probs;
+}
+
+function markovGen(count, probs, prev_count, sep) {
     var prevs = new Array(prev_count).fill('');
     var wordlist = [];
+    if (typeof probs === 'undefined') return '&nbsp';
 
     for (var i = 0; i < count; ++i) {
-        var prev = prevs.join('');
+        var prev = prevs.join(sep);
         var word = markov(prev, probs[prev_count - 1]);
         prevs.shift();
         prevs.push(word);
         wordlist.push(word);
     }
 
-    return wordlist.join('');
+    var output = wordlist.join(sep);
+    if (/^\s*$/.test(output)) {
+        return '&nbsp;'
+    } else {
+        return output;
+    }
+}
+
+function scanCustom() {
+    var text = document.getElementById('custom-text').value;
+    probs_cust = scan(text);
+    probs_cust_char = scanChar(text);
 }
 
 function generate() {
@@ -52,18 +100,32 @@ function generate() {
         case 'recipe':
             text = doRecipe(count, prec);
             break;
+        case 'cust':
+            text = doCustom(count, prec);
+            break;
+        case 'custchar':
+            text = doCustomChars(count, prec);
+            break;
     }
     document.getElementById('output-text').innerHTML = text;
 }
 
 function doRecipe(c, p) {
-    return markov_gen(c < 1 ? 30 : c, probs_recipe, p > 4 ? 4 : p);
+    return markovGen(c < 1 ? 30 : c, probs_recipe, p > 4 ? 4 : p, ' ');
 }
 
 function doLovecraft(c, p) {
-    return markov_gen(c < 1 ? 30 : c, probs, p > 4 ? 4 : p);
+    return markovGen(c < 1 ? 30 : c, probs, p > 4 ? 4 : p, ' ');
 }
 
 function doChars(c, p) {
-    return markov_gen_char(c < 1 ? 200 : c, probs_char, p > 10 ? 10 : p);
+    return markovGen(c < 1 ? 200 : c, probs_char, p > 10 ? 10 : p, '');
+}
+
+function doCustom(c, p) {
+    return markovGen(c < 1 ? 30 : c, probs_cust, p > 4 ? 4 : p, ' ');
+}
+
+function doCustomChars(c, p) {
+    return markovGen(c < 1 ? 200 : c, probs_cust_char, p > 10 ? 10 : p, '');
 }
